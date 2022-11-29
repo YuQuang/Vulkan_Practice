fbec4515522a32a290e29ced49fc0b4eb5b63642
YuQuan <3a817128@gm.student.ncut.edu.tw>
2022-11-29 22:41:37 +0800

    Finished tutorial Device setup & Pipeline stages

--- src/lve_pipeline.hpp
+++ src/lve_pipeline.hpp
@@ -1,16 +1,38 @@
 #pragma one
 
+#include "lve_device.hpp"
+
 #include <string>
 #include <vector>
 
 namespace lve {
+    class PipelineConfigInfo{
+
+    };
+
     class LvePipeline {
         public:
-            LvePipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+            LvePipeline(
+                LveDevice& device,
+                const std::string& vertFilepath,
+                const std::string& fragFilepath,
+                const PipelineConfigInfo& configInfo);
+            ~LvePipeline(){};
+            LvePipeline(const LvePipeline&) = delete;
+            void operator=(const LvePipeline&) = delete;
+
+            static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
         private:
             static std::vector<char> readFile(const std::string& filepath);
 
-            void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+            void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo& configInfo);
+
+            void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
+
+            LveDevice& lveDevice;
+            VkPipeline graphicPipeline;
+            VkShaderModule vertShaderModule;
+            VkShaderModule fragShaderModule;
     };
 }
